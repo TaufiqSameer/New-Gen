15d8708a7739445efe46d19b085d018d0cf07fe8
TaufiqSameer <sameertanker@gmail.com>
2025-10-27 23:33:29 +0530

    Remove Firebase configuration from firebase.js
    Removed Firebase configuration details from the file.

--- src/firebase.js
+++ src/firebase.js
@@ -7,15 +7,6 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCuxQHI__lvsKCsTVla1PqbZdsb6V6K6Bk",
-  authDomain: "newgen-50974.firebaseapp.com",
-  projectId: "newgen-50974",
-  storageBucket: "newgen-50974.firebasestorage.app",
-  messagingSenderId: "20394819851",
-  appId: "1:20394819851:web:d1b513c3065058586c620e",
-  measurementId: "G-YX6R9NNJMD"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
